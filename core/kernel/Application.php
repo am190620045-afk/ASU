3766b0b211ac20fcf3397723f67b634492b386e1
am190620045-afk <am190620045@gmail.com>
2026-07-15 18:48:01 +0300

    ASU v0.2.0-beta: bind runtime configuration to application

--- core/kernel/Application.php
+++ core/kernel/Application.php
@@ -14,16 +14,20 @@ class Application
 {
     private ServiceContainer $container;
     private ModuleManager $modules;
+    private ConfigLoader $config;
 
     public function __construct()
     {
         $this->container = new ServiceContainer();
         $this->modules = new ModuleManager();
+        $this->config = new ConfigLoader();
     }
 
     public function run(): void
     {
-        $this->container->register('config', new ConfigLoader());
+        $this->config->load(dirname(__DIR__, 2) . '/config/app.config.ini');
+
+        $this->container->register('config', $this->config);
         $this->container->register('database', new DatabaseService(new Database()));
         $this->container->register('security', new SecurityService(new Authentication()));
         $this->container->register('modules', $this->modules);
@@ -34,5 +38,10 @@ class Application
     public function services(): ServiceContainer
     {
         return $this->container;
+    }
+
+    public function config(): ConfigLoader
+    {
+        return $this->config;
     }
 }
