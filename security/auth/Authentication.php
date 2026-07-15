aa1d602f7d1c6586cc5428e6df246cb81bfcdf8a
am190620045-afk <am190620045@gmail.com>
2026-07-15 17:48:52 +0300

    ASU v0.2.0: add authentication foundation

---
+++ security/auth/Authentication.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ASU\Security\Auth;
+
+class Authentication
+{
+    public function verify(string $username, string $password): bool
+    {
+        // Authentication service placeholder.
+        return false;
+    }
+}
