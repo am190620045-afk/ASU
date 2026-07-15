6ce7ffcc525054f549a60dd3dfd8951a981a36c2
am190620045-afk <am190620045@gmail.com>
2026-07-15 19:58:55 +0300

    ASU v0.2.0-beta: connect admin module route provider

--- admin/AdminModule.php
+++ admin/AdminModule.php
@@ -2,7 +2,9 @@
 
 namespace ASU\Admin;
 
+use ASU\Core\Http\Router;
 use ASU\Core\Modules\ModuleInterface;
+use ASU\Admin\Dashboard\DashboardController;
 
 class AdminModule implements ModuleInterface
 {
@@ -19,5 +21,12 @@ class AdminModule implements ModuleInterface
     public function boot(): void
     {
         // Initialize admin runtime.
+    }
+
+    public function registerRoutes(Router $router): void
+    {
+        $router->get('/admin', function () {
+            return 'Admin Dashboard';
+        });
     }
 }
