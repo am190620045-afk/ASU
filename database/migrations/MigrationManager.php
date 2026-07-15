69c7ffcd0a2a59ea649c7a1d213c53a5a7cab7c0
am190620045-afk <am190620045@gmail.com>
2026-07-15 17:45:48 +0300

    ASU v0.2.0: add migration manager foundation

---
+++ database/migrations/MigrationManager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ASU\Database\Migrations;
+
+class MigrationManager
+{
+    public function run(array $migrations): void
+    {
+        foreach ($migrations as $migration) {
+            // Migration execution placeholder.
+        }
+    }
+}
