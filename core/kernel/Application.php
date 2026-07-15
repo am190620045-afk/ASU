3b274b1f7675e55cddd58b9d2cbba03dd3f73f4a
am190620045-afk <am190620045@gmail.com>
2026-07-15 17:56:33 +0300

    ASU v0.2.0: integrate core application services

--- core/kernel/Application.php
+++ core/kernel/Application.php
@@ -2,10 +2,26 @@
 
 namespace ASU\Core\Kernel;
 
+use ASU\Core\Config\ConfigLoader;
+use ASU\Core\Services\ServiceContainer;
+use ASU\Modules\Core\ModuleManager;
+
 class Application
 {
+    private ServiceContainer $container;
+    private ModuleManager $modules;
+
+    public function __construct()
+    {
+        $this->container = new ServiceContainer();
+        $this->modules = new ModuleManager();
+    }
+
     public function run(): void
     {
-        // Application lifecycle entry point.
+        $this->container->register('config', new ConfigLoader());
+        $this->container->register('modules', $this->modules);
+
+        $this->modules->boot();
     }
 }
