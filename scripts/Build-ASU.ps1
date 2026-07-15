c6873de646f22e7115e279bfba015bd71201b9ec
am190620045-afk <am190620045@gmail.com>
2026-07-15 16:56:03 +0300

    Initial import: add build script

---
+++ scripts/Build-ASU.ps1
@@ -0,0 +1,7 @@
+# ASU Build System
+# Version: 0.1.0 Build 2026.07.14.003
+# PowerShell 5.1 compatible entry point
+
+$BuildVersion = 'ASU v0.1.0 Build 2026.07.14.003'
+Write-Host $BuildVersion
+Write-Host 'ASU build initialization started.'
