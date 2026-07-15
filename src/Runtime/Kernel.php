076275289478c162edfb703930ffc4bccd697226
am190620045-afk <am190620045@gmail.com>
2026-07-15 20:49:10 +0300

    Integrate runtime container config and module discovery into kernel

--- src/Runtime/Kernel.php
+++ src/Runtime/Kernel.php
@@ -4,18 +4,35 @@ declare(strict_types=1);
 
 namespace ASU\Runtime;
 
+use ASU\Config\Loader;
+use ASU\Module\Discovery;
+
 final class Kernel
 {
     private bool $booted = false;
+    private Container $container;
+
+    public function __construct()
+    {
+        $this->container = new Container();
+    }
 
     public function boot(): array
     {
+        $this->registerServices();
         $this->booted = true;
 
         return [
             'kernel' => 'ready',
             'booted' => $this->booted,
+            'services' => ['config', 'modules'],
             'runtime' => Bootstrap::initialize(),
         ];
+    }
+
+    private function registerServices(): void
+    {
+        $this->container->set('config', new Loader());
+        $this->container->set('modules', new Discovery());
     }
 }
