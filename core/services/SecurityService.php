a7122d9d96418774614f9448c5818c2c05aa7ecb
am190620045-afk <am190620045@gmail.com>
2026-07-15 18:51:07 +0300

    ASU v0.2.0-beta: integrate security user role permission services

--- core/services/SecurityService.php
+++ core/services/SecurityService.php
@@ -3,15 +3,54 @@
 namespace ASU\Core\Services;
 
 use ASU\Security\Auth\Authentication;
+use ASU\Security\Users\UserManager;
+use ASU\Security\Roles\RoleManager;
+use ASU\Security\Permissions\PermissionManager;
+use ASU\Security\Users\User;
 
 class SecurityService
 {
+    private UserManager $users;
+    private RoleManager $roles;
+    private PermissionManager $permissions;
+
     public function __construct(private Authentication $authentication)
     {
+        $this->users = new UserManager();
+        $this->roles = new RoleManager();
+        $this->permissions = new PermissionManager();
     }
 
     public function authenticate(string $username, string $password): bool
     {
         return $this->authentication->verify($username, $password);
+    }
+
+    public function addUser(User $user): void
+    {
+        $this->users->add([
+            'name' => $user->getName(),
+            'roles' => $user->roles()
+        ]);
+    }
+
+    public function hasRole(User $user, string $role): bool
+    {
+        return in_array($role, $user->roles(), true);
+    }
+
+    public function users(): UserManager
+    {
+        return $this->users;
+    }
+
+    public function roles(): RoleManager
+    {
+        return $this->roles;
+    }
+
+    public function permissions(): PermissionManager
+    {
+        return $this->permissions;
     }
 }
