cdc4b452023e2a629d1314d6b2784aa73eddf838
am190620045-afk <am190620045@gmail.com>
2026-07-19 21:13:36 +0300

    feat(kernel): add kernel configuration foundation

---
+++ config/kernel.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'bootstrap' => [
+        'strict' => true,
+    ],
+
+    'services' => [
+        'container' => true,
+        'modules' => true,
+    ],
+];
