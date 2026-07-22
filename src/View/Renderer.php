5447ab9a6a40cb5c3f0334b4cc6405cc06a62371
am190620045-afk <am190620045@gmail.com>
2026-07-23 01:01:57 +0300

    feat: implement view renderer foundation

--- src/View/Renderer.php
+++ src/View/Renderer.php
@@ -4,10 +4,27 @@ declare(strict_types=1);
 
 namespace ASU\View;
 
-final class Renderer
+final class Renderer implements RendererInterface
 {
+    public function __construct(
+        private readonly string $basePath
+    ) {
+    }
+
     public function render(string $template, array $data = []): string
     {
-        return $template;
+        $path = rtrim($this->basePath, '/\\') . '/' . ltrim($template, '/\\');
+
+        if (!is_file($path)) {
+            return '';
+        }
+
+        extract($data, EXTR_SKIP);
+
+        ob_start();
+
+        require $path;
+
+        return (string) ob_get_clean();
     }
 }
