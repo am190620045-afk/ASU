96ede84614054cc938a69fc202eb7d9f24199ad6
am190620045-afk <am190620045@gmail.com>
2026-07-23 11:02:55 +0300

    feat: register database diagnostics endpoint

--- src/Application/Bootstrap.php
+++ src/Application/Bootstrap.php
@@ -6,6 +6,7 @@ namespace ASU\Application;
 
 use ASU\Controller\AuthController;
 use ASU\Controller\DashboardController;
+use ASU\Controller\DatabaseController;
 use ASU\Controller\HomeController;
 use ASU\Controller\RuntimeController;
 use ASU\Http\Request;
@@ -24,6 +25,7 @@ final class Bootstrap
         $authController = $container->get(AuthController::class);
         $dashboardController = $container->get(DashboardController::class);
         $runtimeController = $container->get(RuntimeController::class);
+        $databaseController = $container->get(DatabaseController::class);
 
         $router->add(new Route('GET', '/', static fn (): Response => $homeController->index()));
         $router->add(new Route('GET', '/login', static fn (): Response => $authController->login()));
@@ -31,6 +33,7 @@ final class Bootstrap
         $router->add(new Route('GET', '/logout', static fn (): Response => $authController->logout()));
         $router->add(new Route('GET', '/dashboard', static fn (): Response => $dashboardController->index()));
         $router->add(new Route('GET', '/status/runtime', static fn (): Response => $runtimeController->status()));
+        $router->add(new Route('GET', '/status/database', static fn (): Response => $databaseController->status()));
 
         return new Application($router);
     }
