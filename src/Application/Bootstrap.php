e58e64f7521c0abee541b84c2e8671f970ddd317
am190620045-afk <am190620045@gmail.com>
2026-07-23 10:58:45 +0300

    feat: register runtime diagnostics endpoint

--- src/Application/Bootstrap.php
+++ src/Application/Bootstrap.php
@@ -7,6 +7,7 @@ namespace ASU\Application;
 use ASU\Controller\AuthController;
 use ASU\Controller\DashboardController;
 use ASU\Controller\HomeController;
+use ASU\Controller\RuntimeController;
 use ASU\Http\Request;
 use ASU\Http\Response;
 use ASU\Routing\Route;
@@ -22,12 +23,14 @@ final class Bootstrap
         $homeController = $container->get(HomeController::class);
         $authController = $container->get(AuthController::class);
         $dashboardController = $container->get(DashboardController::class);
+        $runtimeController = $container->get(RuntimeController::class);
 
         $router->add(new Route('GET', '/', static fn (): Response => $homeController->index()));
         $router->add(new Route('GET', '/login', static fn (): Response => $authController->login()));
         $router->add(new Route('POST', '/login', static fn (): Response => $authController->authenticate(Request::fromGlobals())));
         $router->add(new Route('GET', '/logout', static fn (): Response => $authController->logout()));
         $router->add(new Route('GET', '/dashboard', static fn (): Response => $dashboardController->index()));
+        $router->add(new Route('GET', '/status/runtime', static fn (): Response => $runtimeController->status()));
 
         return new Application($router);
     }
