9256f8afc9ef37d361e4bd344bf4d4f78ab645c2
am190620045-afk <am190620045@gmail.com>
2026-07-23 10:54:39 +0300

    feat: resolve middleware dependencies from container

--- src/Runtime/WebApplicationRuntime.php
+++ src/Runtime/WebApplicationRuntime.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace ASU\Runtime;
 
+use ASU\Application\ApplicationFactory;
 use ASU\Application\Bootstrap;
+use ASU\Http\Middleware\AuthMiddleware;
 use ASU\Http\Middleware\MiddlewarePipeline;
 use ASU\Http\Middleware\RuntimeContextMiddleware;
 use ASU\Http\Request;
 use ASU\Http\Response;
+use ASU\Security\AuthGuard;
 
 final class WebApplicationRuntime
 {
@@ -19,8 +22,15 @@ final class WebApplicationRuntime
         $kernel = new Kernel();
         self::$context = new RuntimeContext();
 
+        $container = ApplicationFactory::createContainer();
+
         $pipeline = new MiddlewarePipeline();
         $pipeline->add(new RuntimeContextMiddleware());
+        $pipeline->add(
+            new AuthMiddleware(
+                $container->get(AuthGuard::class)
+            )
+        );
 
         try {
             $kernel->boot();
