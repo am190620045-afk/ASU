a9e3cd91b84d91b5ff9717cb4a12618aa0c975ab
am190620045-afk <am190620045@gmail.com>
2026-07-23 09:04:40 +0300

    feat: add login template

---
+++ templates/auth/login.php
@@ -0,0 +1,15 @@
+<h1>ASU Login</h1>
+
+<form method="post">
+    <label>
+        Username
+        <input name="username" type="text">
+    </label>
+
+    <label>
+        Password
+        <input name="password" type="password">
+    </label>
+
+    <button type="submit">Login</button>
+</form>
