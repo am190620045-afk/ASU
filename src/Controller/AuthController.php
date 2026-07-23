f7fd3407f23ad18ab4ea3d0d0af3e0053d38783d
am190620045-afk <am190620045@gmail.com>
2026-07-23 09:35:44 +0300

    feat: add login authentication handler

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace ASU\Controller;
 
+use ASU\Http\Request;
 use ASU\Http\Response;
 use ASU\Security\Authenticator;
 use ASU\View\RendererInterface;
@@ -21,6 +22,20 @@ final class AuthController extends Controller
         return new Response(
             $this->renderer->render('auth/login.php')
         );
+    }
+
+    public function authenticate(Request $request): Response
+    {
+        $success = $this->authenticator->login(
+            (string) $request->input('username', ''),
+            (string) $request->input('password', '')
+        );
+
+        if (!$success) {
+            return new Response('Authentication failed', 401);
+        }
+
+        return new Response('Logged in');
     }
 
     public function logout(): Response
